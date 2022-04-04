b1ae215afcc88ed321b7a1a3f3203f24e72d99ff
ThatGuyJamal <jamallyons8@gmail.com>
2022-04-04 14:15:33 -0400

    🐦Removing maps

--- phoenix.go
+++ phoenix.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/phoenix-language/phoenix/cli"
-	"github.com/phoenix-language/phoenix/util"
 )
 
 var output = "Phoenix, Programming Reborn...🐦"
 
 func main() {
-
-	langMetaData := util.SyncMap[string, string]{}
-
-	langMetaData.Store("version", "0.0.1")
-	langMetaData.Store("developers", "Phoenix Development Team")
-
-	fmt.Println(langMetaData.Load("version"))
-
 	fmt.Println(output)
 
 	cli.ExecuteClI()
