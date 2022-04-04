2075a273e3be390b3ce28d4a0e2ed8148d0a0e46
ThatGuyJamal <jamallyons8@gmail.com>
2022-04-04 15:44:39 -0400

    🐦MIsc

--- phoenix.go
+++ phoenix.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/phoenix-language/phoenix/cli"
 )
 
 var output = "Phoenix, Programming Reborn...🐦"
 
 func main() {
 	fmt.Println(output)
-
-	cli.ExecuteClI()
 }
