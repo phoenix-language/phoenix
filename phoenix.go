0af37568556b5f415fcec3daebbe22ab4c835219
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-04-04 07:29:24 -0400

    🐦golang

--- phoenix.go
+++ phoenix.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var output = "Phoenix, Programming Reborn...🐦"
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(output)
+
+	file, err := os.Open("README.md") // For read access.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := make([]byte, 10000)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("read %d bytes: %q\n", count, data[:count])
 }
